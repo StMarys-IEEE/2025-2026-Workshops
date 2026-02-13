bdbc7fe9cf054cb48f55f2d5e0665ff9f931d395
Nick Galus <25nickg@gmail.com>
2026-02-13 10:37:08 -0600

    Updates to input detection to use bitmapping

--- Robotic Arm/servo.py
+++ Robotic Arm/servo.py
@@ -2,11 +2,38 @@ import serial
 import keyboard
 import time
 
-ser = serial.Serial('COM7', 9600)  # change COM port
+# 1) Connect to Arduino
+ser = serial.Serial('COM7', 9600)
+time.sleep(2)  # Arduino often resets when serial opens
+
+# 2) Constants
+START = 0xAA
+FRAME_DT = 0.02  # 20 ms = 50 Hz
+
+# 3) 50 Hz loop
+next_t = time.perf_counter()
 
 while True:
-    if keyboard.is_pressed('a'):
-        ser.write(b'a')
-    elif keyboard.is_pressed('d'):
-        ser.write(b'd')
-    #time.sleep(0.05)
+    flags = 0
+
+    # Bit mapping (8 keys -> 8 bits)
+    # bit0 W, bit1 S, bit2 A, bit3 D, bit4 left, bit5 right, bit6 up, bit7 down
+    if keyboard.is_pressed('w'):     flags |= (1 << 0)
+    if keyboard.is_pressed('s'):     flags |= (1 << 1)
+    if keyboard.is_pressed('a'):     flags |= (1 << 2)
+    if keyboard.is_pressed('d'):     flags |= (1 << 3)
+    if keyboard.is_pressed('left'):  flags |= (1 << 4)
+    if keyboard.is_pressed('right'): flags |= (1 << 5)
+    if keyboard.is_pressed('up'):    flags |= (1 << 6)
+    if keyboard.is_pressed('down'):  flags |= (1 << 7)
+
+    # Send exactly once per frame (start byte + flags byte)
+    ser.write(bytes([START, flags]))
+
+    # Wait until the next 20ms boundary
+    next_t += FRAME_DT
+    sleep_time = next_t - time.perf_counter()
+    if sleep_time > 0:
+        time.sleep(sleep_time)
+    else:
+        next_t = time.perf_counter()
